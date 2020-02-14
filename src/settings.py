c65e301456fb761a3bdea01495deba0b72bdf098
Guilherme <guilherme.lopes@engenharia.ufjf.br>
2020-02-14 20:21:03 -0200

    Adjusting settings and requirements
    Requirements file was changed to add new packages

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ class FilterSettings:
         self.sup = 16
         self.inf = -26
         self.roc_grid = 120
-        self.w_range = [1, 3, 5, 7, 9, 11, 13]
-        self.filters = ['unsupervised_wiener']
+        self.w_range = [1, 3, 5, 7, 9]
+        self.filters = ['unsupervised_wiener','gauss','mean','median']
         self.output_file_path = '../data/'
-        self.output_file_name = 'result_test_wierner'
+        self.output_file_name = 'result_all_filters'
