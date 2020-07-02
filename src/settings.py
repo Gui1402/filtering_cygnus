4a4114c1e1bf1269548c1818c119a877c38e2c71
guilherme.pinheiro@santodigital.com.br <guilherme.pinheiro@santodigital.com.br>
2020-07-02 09:38:09 -0300

    add filter parameters

--- src/settings.py
+++ src/settings.py
@@ -14,20 +14,20 @@ class FilterSettings:
         self.filters = {
 
                         #'bilateral': [[i, j, k] for i in range(3, 19, 2) for j in range(1, 5, 2) for k in range(1, 19, 2)],
-                        #'nlmeans': [[i, j] for i in range(1, 17, 2) for j in range(1, 17, 2)],
+                        'nlmeans': [[i, j] for i in range(1, 30, 2) for j in range(1, 30, 2)],
                         #'nlmeans':[[11, 7]],
-                        'mean': [[3], [7], [13], [17], [21]],
-                        'gaussian': [[3], [7], [13], [17], [21]],
+                        #'mean': [[3], [7], [13], [17], [21]],
+                        #'gaussian': [[3], [7], [13], [17], [21]],
                         #'mean': [[7]],
-                        #'median': [[3], [5], [7], [9], [11], [13], [15]],
+                        'median': [[3], [5], [7], [9], [11], [13], [15], [17], [19], [21], [23], [25], [27]],
                         #'wiener': [[1], [3]],
-                        #'bm3D': [[3]],
+                        'bm3D': [[1], [2], [3], [4], [5], [6], [7], [8]],
                         #'FCAIDE': [[1], [2], [3], [4], [5]],
                         #'DnCNN': [[self.output_file_path + 'snapshot/DnCNN_Net/filtering_cygnus_snapshot_save_DnCNN_run817_2020-03-19-19-19-14_model_40.h5'],
                         #          [self.output_file_path + 'snapshot/DnCNN_Net/model_50.hdf5'],
                         #          [self.output_file_path + 'snapshot/BRD_Net/15/model_50.h5'],
                         #          [self.output_file_path + 'snapshot/BRD_Net/25/model_50.h5'],
                         #          [self.output_file_path + 'snapshot/BRD_Net/50/model_50.h5']]
-                        'cygno': [],
+                        #'cygno': [],
                         }
 
